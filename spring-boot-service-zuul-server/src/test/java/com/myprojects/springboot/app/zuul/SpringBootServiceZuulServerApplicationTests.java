8bdf0c44d1b0f21f0b6c29289c495797ad36a68d
Jorge Pinzon <jpinzon95@hotmail.com>
2024-08-13 22:31:55 -0500

    Added spring-cloud-starter-gateway and routes for access every µservices.

---
+++ spring-boot-service-zuul-server/src/test/java/com/myprojects/springboot/app/zuul/SpringBootServiceZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojects.springboot.app.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootServiceZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
