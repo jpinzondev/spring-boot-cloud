2f22582bcf57229c70888349bf16dc8160fbcc4e
Jorge Pinzon <jpinzon95@hotmail.com>
2024-09-23 22:28:13 -0500

    [Adding config server and config files for items and products µservices]

---
+++ spring-boot-service-config-server/src/test/java/com/myprojects/springboot/app/config/SpringBootServiceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojects.springboot.app.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootServiceConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- springboot-servicio-item/src/main/java/com/myprojects/springboot/app/item/controller/ItemController.java
+++ springboot-servicio-item/src/main/java/com/myprojects/springboot/app/item/controller/ItemController.java
@@ -8,10 +8,15 @@ import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
@@ -24,6 +29,12 @@ public class ItemController {
 
     @Autowired
     private ItemService itemService;
+
+    @Value("${configuration.text}")
+    private String text;
+
+    @Value("${server.port}")
+    private String port;
 
     @GetMapping("/v1/list")
     public List<Item> list(
@@ -78,5 +89,14 @@ public class ItemController {
         item.setProduct(product);
         logger.error(e.toString());
         return CompletableFuture.supplyAsync(() ->item);
+    }
+
+    @GetMapping("/get-config")
+    public ResponseEntity<?> getConfig() {
+        logger.info(text);
+        Map<String, String> map = new HashMap<>();
+        map.put("text", text);
+        map.put("port", port);
+        return new ResponseEntity<Map<String, String>>(map, HttpStatus.OK);
     }
 }
